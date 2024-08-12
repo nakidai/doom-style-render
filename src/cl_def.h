d266bca25c49663950e82953c1de8eca7c0f3994
glebm <glebmuravlev0511@gmail.com>
2024-08-12 21:43:05 +0300

    header limits.h included in u_def.h

--- src/cl_def.h
+++ src/cl_def.h
@@ -1,6 +1,7 @@
 #ifndef _cl_def_h
 #define _cl_def_h
 
+#include "utils/u_def.h"
 #include "lib/cbmp/cbmp.h"
 #include "game/g_player.h"
 #include "render/r_render.h"
@@ -10,7 +11,6 @@
 #include "core/file/f_wad.h"
 #include "game/g_map.h"
 #include "core/file/f_conf.h"
-#include "utils/u_def.h"
 #include "core/mem/m_alloc.h"
 #include "core/mem/m_utils.h"
 #include "render/draw/r_draw.h"
