80f665d276444d7d90f97e5478731ecfa45f853e
glebm <glebmuravlev0511@gmail.com>
2024-08-13 15:20:06 +0300

    added diseling effect

--- src/cl_main.c
+++ src/cl_main.c
@@ -1,7 +1,8 @@
 #include "cl_def.h"
 
-state_t g_cState;
+state_t              g_cState;
 extern playerstate_t g_cPlayerstate;
+extern vidstate_t    g_cVidstate;
 
 static void CL_Init(int argc, char** argv) {
 #   ifdef DEBUG_PRINT_INIT
@@ -48,6 +49,14 @@ static void CL_MainLoop(void) {
 
         V_Update();
         R_Render();
+
+        for (int i = 0; i < SCREEN_WIDTH; i++) {
+            for (int j = 0; j < SCREEN_HEIGHT; j++) {
+                const u32 color = g_cVidstate.pixels[j * SCREEN_WIDTH + i];
+                g_cVidstate.pixels[j * SCREEN_WIDTH + i] = (i % 2 == 0) && (j % 2 == 0) ? R_AbgrMul(color, 192) : color;
+            }
+        }
+
         if (!g_cState.sleepy) { V_Present(); }
     }
 }
