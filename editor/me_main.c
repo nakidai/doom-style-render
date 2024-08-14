07d50149a4fb3bcde1d4543530012e1c7df34e03
glebm <glebmuravlev0511@gmail.com>
2024-08-14 16:38:42 +0300

    added command system for level editor

--- editor/me_main.c
+++ editor/me_main.c
@@ -31,6 +31,9 @@ static void ME_DrawString(int x, int y, const char* line) {
 #   endif
 }
 
+bool draw_wall_num = true;
+bool draw_sect_num = true;
+
 static void ME_Render(void) {
     for (int i = 0; i < g_cState.map.walls.n; i++) {
         const wall_t* wall = &g_cState.map.walls.arr[i];
@@ -46,34 +49,58 @@ static void ME_Render(void) {
             (wall->b.x + pos.x) * (int) scale,
             (wall->b.y + pos.y) * (int) scale
         );
-    }
 
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        if (draw_wall_num) {
+            SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
 
-    for (int i = 1; i < g_cState.map.sectors.n; i++) {
-        const sector_t* sector = &g_cState.map.sectors.arr[i];
-        v2i center = { 0, 0 };
-
-        for (int j = sector->firstwall; j < sector->nwalls + sector->firstwall; j++) {
-            const wall_t* wall = &g_cState.map.walls.arr[j];
-
+            v2i center = { 0, 0 };
             center.x += wall->a.x;
             center.y += wall->a.y;
             center.x += wall->b.x;
             center.y += wall->b.y;
+
+            center.x /= 2;
+            center.y /= 2;
+
+            char num[10];
+            sprintf(num, "%i", i);
+
+            ME_DrawString(
+                (center.x + pos.x) * (int) scale,
+                (center.y + pos.y) * (int) scale,
+                num
+            );
         }
+    }
 
-        center.x /= sector->nwalls * 2;
-        center.y /= sector->nwalls * 2;
+    if (draw_sect_num) {
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 
-        char num[10];
-        sprintf(num, "%i", sector->id);
+        for (int i = 1; i < g_cState.map.sectors.n; i++) {
+            const sector_t* sector = &g_cState.map.sectors.arr[i];
+            v2i center = { 0, 0 };
 
-        ME_DrawString(
-            (center.x + pos.x) * (int) scale,
-            (center.y + pos.y) * (int) scale,
-            num
-        );
+            for (int j = sector->firstwall; j < sector->nwalls + sector->firstwall; j++) {
+                const wall_t* wall = &g_cState.map.walls.arr[j];
+
+                center.x += wall->a.x;
+                center.y += wall->a.y;
+                center.x += wall->b.x;
+                center.y += wall->b.y;
+            }
+
+            center.x /= sector->nwalls * 2;
+            center.y /= sector->nwalls * 2;
+
+            char num[10];
+            sprintf(num, "%i", sector->id);
+
+            ME_DrawString(
+                (center.x + pos.x) * (int) scale,
+                (center.y + pos.y) * (int) scale,
+                num
+            );
+        }
     }
 }
 
@@ -89,8 +116,52 @@ static void ME_ProcessEvents(void) {
     if (keystate[SDLK_RIGHT & 0xFFFF]) pos.x -= MOVE_SPEED;
 }
 
-int main() {
+int ME_AddPoint(char* args) {
+    v2i pos = { NAN, NAN };
+
+    if (*args == 'c') {
+        int x, y;
+        SDL_GetMouseState(&x, &y);
+
+        pos.x = x / scale;
+        pos.y = y / scale;
+
+        goto done;
+    } else {
+        if (sscanf(
+            args,
+            "%i %i",
+            &pos.x,
+            &pos.y
+        ) != 2) return 2;
+
+        goto done;
+    }
+
+    done:
+        return SUCCESS;
+}
+
+int ME_Scale(char* args) {
+    if (sscanf(
+        args,
+        "%f",
+        &scale
+    ) != 1) return 2;
+    return SUCCESS;
+}
+
+static void ME_InitCommands(void) {
+    CMD_AddCommand("ap", &ME_AddPoint);
+    CMD_AddCommand("s", &ME_Scale);
+}
+
+static char console_buf[64] = {};
+
+int main(int argc, char* argv[]) {
+    SYS_Init(argc, argv);
     M_Init();
+    CMD_Init();
 
     ASSERT(
         !SDL_Init(SDL_INIT_VIDEO),
@@ -128,12 +199,34 @@ int main() {
     W_LoadWAD(&g_cState.wad);
     G_LoadMap(&g_cState.map, "TEST");
 
+    ME_InitCommands();
+
     while (true) {
         SDL_Event ev;
 
         while (SDL_PollEvent(&ev)) {
             switch (ev.type) {
                 case SDL_QUIT: goto exit;
+                case SDL_TEXTINPUT:
+                    strcat(console_buf, ev.text.text);
+                    break;
+
+                case SDL_KEYDOWN: {
+                    if (ev.key.keysym.sym != SDLK_RETURN) break;
+
+                    printf("push command: %s\n", console_buf);
+
+                    char* buf_ptr = M_TempAlloc(strlen(console_buf));
+                    memcpy(buf_ptr, console_buf, strlen(console_buf));
+                    memset(buf_ptr + strlen(console_buf), '\0', sizeof(console_buf) - strlen(console_buf));
+
+                    CMD_ExecuteText(buf_ptr);
+                    M_TempFree(buf_ptr);
+                    memset(console_buf, '\0', sizeof(console_buf));
+
+                    break;
+                }
+
                 default: break;
             }
         } 
@@ -142,6 +235,10 @@ int main() {
         SDL_RenderClear(renderer);
 
         ME_Render();
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        ME_DrawString(10, 10, console_buf);
+
         ME_ProcessEvents();
 
         SDL_RenderPresent(renderer);
@@ -150,5 +247,6 @@ int main() {
     exit:
     
     M_Free();
+    CMD_Init();
     return 0;
 }
