a90304432bc897f8be6307effc2681b53ff17dde
glebm <glebmuravlev0511@gmail.com>
2024-08-15 03:12:43 +0300

    added variable support in console

--- src/cmd/cmd_exec.c
+++ src/cmd/cmd_exec.c
@@ -5,8 +5,10 @@ typedef struct {
     char     name[64];
 } cmd_t;
 
-static cmd_t cmds[1024] = { NULL };
-static usize cmd_len = 0;
+static cmd_t      cmds[1024]     = { NULL };
+static cmd_var_t* cmd_vars[1024] = { NULL };
+static usize      cmd_len     = 0;
+static usize      cmd_var_len = 0;
 
 void CMD_Init(void) {
 
@@ -24,14 +26,33 @@ void CMD_AddCommand(const char* name, cmd_fn_t func) {
     cmds[cmd_len++] = cmd;
 }
 
+void CMD_AddVariable(cmd_var_t* var) {
+    cmd_vars[cmd_var_len++] = var;
+}
+
 int CMD_ExecuteText(char* text) {
-    int ret = WTF_WHY;
+    int ret = VARIABLE;
 
     const char* name = strtok(text, " ");
 
     for (usize i = 0; i < cmd_len; i++) {
         if (strcmp(cmds[i].name, name) == 0) {
             ret = cmds[i].fn(strtok(NULL, ""));
+            goto done;
+        }
+    }
+
+    for (usize i = 0; i < cmd_var_len; i++) {
+        if (strcmp(cmd_vars[i]->name, name) == 0) {
+            const char* value = strtok(NULL, "");
+
+            if (value == NULL) {
+                char buf[32];
+                snprintf(buf, sizeof(buf), "variable %s is %s\n", name, cmd_vars[i]->value);
+                CON_Printf(buf);
+            }
+            else memcpy(cmd_vars[i]->value, value, strlen(value));
+
             goto done;
         }
     }
--- src/cmd/cmd_exec.h
+++ src/cmd/cmd_exec.h
@@ -5,14 +5,19 @@
 
 #define SUCCESS           0
 #define COMMAND_NOT_FOUND 1
-#define WTF_WHY           255
+#define VARIABLE          255
 
 typedef int (*cmd_fn_t)(char*);
+typedef struct {
+    char name[32];
+    char value[64];
+} cmd_var_t;
 
 void CMD_Init(void);
 void CMD_Free(void);
 
 void CMD_AddCommand(const char* name, cmd_fn_t func);
+void CMD_AddVariable(cmd_var_t* var);
 int CMD_ExecuteText(char* text);
 
 #endif
