7108144aee5b98f4406bc82cbe3e8897a2b297f5
glebm <glebmuravlev0511@gmail.com>
2024-08-15 03:12:43 +0300

    added variable support in console

--- src/console/c_con.c
+++ src/console/c_con.c
@@ -3,6 +3,8 @@
 char con_buf[1024];
 static char con_in[32];
 
+cmd_var_t test_variable = { "test", "this is test variable" };
+
 int CMD_Echo(char* args) {
     char buf[64];
     snprintf(buf, sizeof(buf), "%s\n", args);
@@ -15,6 +17,7 @@ void CON_Init(void) {
     CON_DrawInit();
 
     CMD_AddCommand("echo", &CMD_Echo);
+    CMD_AddVariable(&test_variable);
 
     CON_Printf("console init done!\n");
 }
@@ -24,8 +27,12 @@ void CON_Free(void) {
 }
 
 void CON_Draw(void) {
-    CON_DrawString((v2i) { 10, SCREEN_HEIGHT - 10 }, con_buf);
-    CON_DrawString((v2i) { 10, 10 }, con_in);
+    CON_DrawString((v2i) { 5, SCREEN_HEIGHT - 10 }, con_buf);
+
+    char buf[64];
+    snprintf(buf, sizeof(buf), "> %s", con_in);
+
+    CON_DrawString((v2i) { 10, 10 }, buf);
 }
 
 void CON_Printf(const char* msg) {
@@ -38,8 +45,11 @@ void CON_ProcessInput(const char* text) {
 }
 
 void CON_Exec(void) {
+    if (strlen(con_in) == 0) return;
+
     char* buf_ptr = M_TempAlloc(strlen(con_in));
     memcpy(buf_ptr, con_in, strlen(con_in));
+    memset(buf_ptr + strlen(con_in), '\0', sizeof(con_in) - strlen(con_in));
 
     char out_buf[32];
     snprintf(out_buf, sizeof(out_buf), "Command exited with code %i\n", CMD_ExecuteText(buf_ptr));
