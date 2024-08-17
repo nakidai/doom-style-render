3e1533714be971ce16778111ddc0741338b45583
glebm <glebmuravlev0511@gmail.com>
2024-08-17 15:44:09 +0300

    added comments for cmd_exec.h and cmd_exec.c

--- src/cmd/cmd_exec.c
+++ src/cmd/cmd_exec.c
@@ -1,94 +1,117 @@
-#include "../cl_def.h"
+// Copyright (C) by gimura 2024
+// --- PREFIX ---
+//
+// CMD_* - working with commands
+//
+// --- DESCRIPTION ---
+//
+// command executor
+//
+// -------------------
 
+#include "cl_def.h"
+
+// command type
 typedef struct {
-    cmd_fn_t fn;
-    char     name[64];
+    cmd_fn_t fn;       // pointer to command function
+    char     name[64]; // name of command
 } cmd_t;
 
-static cmd_t      cmds[128]     = { NULL };
-static cmd_var_t* cmd_vars[256] = { NULL };
-static usize      cmd_len     = 0;
-static usize      cmd_var_len = 0;
+static cmd_t      cmds[128]     = { NULL }; // command list
+static cmd_var_t* cmd_vars[256] = { NULL }; // variables list
+static usize      cmd_len     = 0; // count of commands
+static usize      cmd_var_len = 0; // count of variables
 
+// builtin console commands
 int CMD_Echo(char* args);
 int CMD_ExecCommand(char* args);
 
+// init builtin console commands
 void CMD_Init(void) {
     CMD_AddCommand("echo", &CMD_Echo);
     CMD_AddCommand("exec", &CMD_ExecCommand);
 }
 
+// add new command
 void CMD_AddCommand(const char* name, cmd_fn_t func) {
-    cmd_t cmd = {};
-    memcpy(cmd.name, name, strlen(name));
-    cmd.fn   = func;
+    cmd_t cmd = {};                       // create command structure
+    memcpy(cmd.name, name, strlen(name)); // copy name to structure
+    cmd.fn = func;                        // set function pointer to target function
 
-    cmds[cmd_len++] = cmd;
+    cmds[cmd_len++] = cmd; // push command structure to last free index
 }
 
+// add console variable
 void CMD_AddVariable(cmd_var_t* var) {
-    cmd_vars[cmd_var_len++] = var;
+    cmd_vars[cmd_var_len++] = var; // push variable to last free index
 }
 
+// execute text
 int CMD_ExecuteText(const char* in) {
-    if (*in == '#' || in == NULL || *in == '\0') return COMMENT;
+    if (*in == '#' || in == NULL || *in == '\0') return COMMENT; // if comment or null string, return
 
-    int ret = VARIABLE;
+    int ret = VARIABLE; // set return status to variable
 
-    char text[64] = {'\0'};
-    memcpy(text, in, strlen(in));
-    text[strlen(in)] = '\0';
+    char text[64] = {'\0'};       // create buffer for strtok
+    memcpy(text, in, strlen(in)); // copy in variable to buffer
+    text[strlen(in)] = '\0';      // set last char to '\0'
 
-    const char* name = strtok(text, " ");
+    const char* name = strtok(text, " "); // get name 
 
     for (usize i = 0; i < cmd_len; i++) {
+        // if name and command name in index i equals, execute command fuction
         if (strcmp(cmds[i].name, name) == 0) {
-            ret = cmds[i].fn(strtok(NULL, ""));
+            ret = cmds[i].fn(strtok(NULL, "")); // run command fuction
             goto done;
         }
     }
 
     for (usize i = 0; i < cmd_var_len; i++) {
+        // if name and variable name in index i equals
         if (strcmp(cmd_vars[i]->name, name) == 0) {
-            const char* value = strtok(NULL, "");
+            const char* value = strtok(NULL, ""); // get all after name
 
+            // if value is NULL, print variable value
             if (value == NULL) {
                 char buf[32];
                 snprintf(buf, sizeof(buf), "variable %s is %s\n", name, cmd_vars[i]->value);
                 CON_Printf(buf);
             }
-            else memcpy(cmd_vars[i]->value, value, strlen(value));
+            else memcpy(cmd_vars[i]->value, value, strlen(value)); // else copy value to variable value
 
             goto done;
         }
     }
 
-    ret = COMMAND_NOT_FOUND;
+    ret = COMMAND_NOT_FOUND; // if command or variable with name not found, set ret to command not found value 
 
     done:
-        return ret;
+        return ret; // return result
 }
 
+// echo command
 int CMD_Echo(char* args) {
+    // add \n to end
     char buf[64];
     snprintf(buf, sizeof(buf), "%s\n", args);
 
-    CON_Printf(buf);
+    CON_Printf(buf); // print variable
     return SUCCESS;
 }
 
+// exec command
 int CMD_ExecCommand(char* args) {
-    FILE* file = fopen(args, "r");
+    FILE* file = fopen(args, "r"); // open file in arguments
+    if (file == NULL) return 2; // if file not found, return
 
-    if (file == NULL) return 2;
-
+    // execute all lines from file
     char line[64];
     while (fgets(line, 64, file)) {
-        if (line[strlen(line) - 1] == '\n') line[strlen(line) - 1] = '\0';
-        CMD_ExecuteText(line);
+        if (line[strlen(line) - 1] == '\n') line[strlen(line) - 1] = '\0'; // remove \n in end
+        CMD_ExecuteText(line);                                             // execute
     }
 
-    fclose(file);
+    fclose(file); // close file
     
     return SUCCESS;
 }
--- src/cmd/cmd_exec.h
+++ src/cmd/cmd_exec.h
@@ -1,26 +1,37 @@
+// Copyright (C) by gimura 2024
+// --- PREFIX ---
+//
+// CMD_* - working with commands
+//
+// --- DESCRIPTION ---
+//
+// command executor
+//
+// -------------------
+
 #ifndef _cmd_exec_h
 #define _cmd_exec_h
 
-#include "../utils/u_def.h"
+#include "utils/u_def.h"
 
-#define SUCCESS           0
-#define COMMAND_NOT_FOUND 1
-#define VARIABLE          255
-#define COMMENT           256
+#define SUCCESS           0   // success command exit code
+#define COMMAND_NOT_FOUND 1   // command not found
+#define VARIABLE          255 // variable exit code
+#define COMMENT           256 // comment exit code
 
-#define READ_FLOAT_VAR(x) ({ if (sscanf(args, "%f", &x) != 1) return 2; return SUCCESS; })
-#define READ_INT_VAR(x)   ({ if (sscanf(args, "%i", &x) != 1) return 2; return SUCCESS; })
+#define READ_FLOAT_VAR(x) ({ if (sscanf(args, "%f", &x) != 1) return 2; return SUCCESS; }) // macro from read float variable
+#define READ_INT_VAR(x)   ({ if (sscanf(args, "%i", &x) != 1) return 2; return SUCCESS; }) // macro from read int variable
 
-typedef int (*cmd_fn_t)(char*);
+typedef int (*cmd_fn_t)(char*); // command fuction
 typedef struct {
-    char name[32];
-    char value[64];
-} cmd_var_t;
+    char name[32];  // variable name
+    char value[64]; // variable value
+} cmd_var_t; // variable
 
-void CMD_Init(void);
+void CMD_Init(void); // init console
 
-void CMD_AddCommand(const char* name, cmd_fn_t func);
-void CMD_AddVariable(cmd_var_t* var);
-int CMD_ExecuteText(const char* text);
+void CMD_AddCommand(const char* name, cmd_fn_t func); // add command
+void CMD_AddVariable(cmd_var_t* var);                 // add variable
+int CMD_ExecuteText(const char* text);                // execute text
 
 #endif
