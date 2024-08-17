7992f70e91c98f9be27cd2b6fd638ea1af02aae9
glebm <glebmuravlev0511@gmail.com>
2024-08-17 18:22:44 +0300

    WAD files removed, now use file system

--- src/core/sys.c
+++ src/core/sys.c
@@ -22,3 +22,67 @@ char* SYS_GetArg(const char* name) {
 
     done: return out;
 }
+
+#ifdef DEV_BUILD
+#define DEFAULT_BASE_DIR "./../../"
+#define DEFAULT_GAME_DIR "res/"
+#else
+#define DEFAULT_BASE_DIR "./"
+#define DEFAULT_GAME_DIR "game/"
+#endif
+
+char* SYS_GetGameDir(void) {
+    char out[64];
+    memset(out, '\0', sizeof(out));
+
+    const char* base_dir = SYS_GetArg("-baseDir");
+    const char* game_dir = SYS_GetArg("-gameDir");
+
+    strcat(out, base_dir == NULL ? DEFAULT_BASE_DIR : base_dir);
+    strcat(out, game_dir == NULL ? DEFAULT_GAME_DIR : game_dir);
+
+    char* buf = M_TempAlloc(strlen(out));
+    memcpy(buf, out, strlen(out));
+
+    return buf;
+}
+
+u8* SYS_ReadFile(const char* name) {
+    char  path[64];
+    char* game_dir = SYS_GetGameDir();
+
+    sprintf(path, "%s%s", game_dir, name);
+
+    M_TempFree(game_dir);
+
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) ERROR("SYS_ReadFile: file %s not found!", path);
+
+    fseek(file, 0L, SEEK_END);
+    const usize size = ftell(file);
+    rewind(file);
+
+    u8* data = M_TempAlloc(size);
+    fread(data, size, 1, file);
+    fclose(file);
+
+    return data;
+}
+
+usize SYS_ReadFileSize(const char* name) {
+    char  path[64];
+    char* game_dir = SYS_GetGameDir();
+
+    sprintf(path, "%s%s", game_dir, name);
+
+    M_TempFree(game_dir);
+
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) ERROR("SYS_ReadFileSize: file %s not found!", path);
+
+    fseek(file, 0L, SEEK_END);
+    const usize size = ftell(file);
+    fclose(file);
+
+    return size;
+}
--- src/game/g_map.c
+++ src/game/g_map.c
@@ -1,60 +1,13 @@
 #include "../cl_def.h"
 
-typedef struct {
-    char geo_txt_dir[8];
-    char geo_bin_dir[8];
-} map_header_t;
-
 extern state_t g_cState;
-
-static void G_LoadMapHeader(map_header_t* header, const char* name) {
-    char map_name[8];
-    snprintf(map_name, sizeof(map_name), "MD_%s", name);
-
-    const wad_directory_t dir = W_FindDirectory(&g_cState.wad, map_name);
-    u8* buf = M_TempAlloc(dir.lump_size);
-    memset(buf, '\0', dir.lump_size);
-    W_ReadWADDirectory(&g_cState.wad, dir, buf);
-    buf[dir.lump_size] = '\0';
-
-    const char* geo_txt_dir = C_GetVariable((char*) buf, "geo_txt");
-    const char* geo_bin_dir = C_GetVariable((char*) buf, "geo_bin");
-
-    M_TempFree(buf);
-
-    if (geo_bin_dir != NULL) goto bin;
-    if (geo_txt_dir != NULL) goto text;
-    goto fail;
-
-    bin:
-        memcpy(header->geo_bin_dir, geo_bin_dir, strlen(geo_bin_dir));
-        goto done;
-
-    text:
-        memcpy(header->geo_txt_dir, geo_txt_dir, strlen(geo_txt_dir));
-        goto done;
-
-    fail:
-        if (geo_bin_dir != NULL) M_TempFree((void*) geo_bin_dir);
-        if (geo_txt_dir != NULL) M_TempFree((void*) geo_txt_dir);
-
-        ERROR("G_LoadHeader: in %s map header not found geometry variable (geo_bin or geo_txt)", name);
-
-    done:
-        if (geo_bin_dir != NULL) M_TempFree((void*) geo_bin_dir);
-        if (geo_txt_dir != NULL) M_TempFree((void*) geo_txt_dir);
-}
 
 static void G_LoadTextWalls(map_t* map, const char* source);
 static void G_LoadTextSectors(map_t* map, const char* source);
 
-static void G_LoadTextGeometry(map_t* map, const map_header_t header) {
-    const wad_directory_t dir = W_FindDirectory(&g_cState.wad, header.geo_txt_dir);
-    u8* buf = M_TempAlloc(dir.lump_size);
-    W_ReadWADDirectory(&g_cState.wad, dir, buf);
-
-    u8* s_buf = M_TempAlloc(dir.lump_size);
-    memcpy(s_buf, buf, dir.lump_size);
+static void G_LoadTextGeometry(map_t* map, u8* buf, usize size) {
+    u8* s_buf = M_TempAlloc(size);
+    memcpy(s_buf, buf, size);
 
     G_LoadTextWalls(map, (char*) buf);
     G_LoadTextSectors(map, (char*) s_buf);
@@ -113,11 +66,8 @@ static void G_LoadTextSectors(map_t* map, const char* source) {
 // }
 
 void G_LoadMap(map_t* map, const char* name) {
-    map_header_t header = {};
+    char path[64];
+    sprintf(path, "maps/%s.prt", name);
 
-    G_LoadMapHeader(&header, name);
-
-    // if (*header.geo_bin_dir != '\0') G_LoadBinGeometry(map, header);
-    if (*header.geo_bin_dir != '\0') ERROR("G_LoadBinGeometry: not implemented");
-    if (*header.geo_txt_dir != '\0') G_LoadTextGeometry(map, header);
+    G_LoadTextGeometry(map, SYS_ReadFile(path), SYS_ReadFileSize(path));
 }
