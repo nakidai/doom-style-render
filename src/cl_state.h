f54a7c1b0253bf2ce4fe03b1122b30401699bdf8
glebm <glebmuravlev0511@gmail.com>
2024-08-17 03:46:34 +0300

    u_state.h moved to src dir and renamed to cl_state.h

--- src/cl_state.h
+++ src/cl_state.h
@@ -8,10 +8,10 @@
 #ifndef _u_state_h
 #define _u_state_h
 
-#include "u_def.h"
-#include "../game/g_map.h"
-#include "../core/file/f_wad.h"
-#include "../render/utils/r_texture.h"
+#include "utils/u_def.h"
+#include "game/g_map.h"
+#include "core/file/f_wad.h"
+#include "render/utils/r_texture.h"
 
 // state type
 typedef enum {
